7f00b8bd7c56b4278de568042930128c79b5083f
Philipp Angerer <phil.angerer@gmail.com>
2018-05-17 13:50:49 +0000

    version 0.5.0

--- R/utils.r
+++ R/utils.r
@@ -25,3 +25,27 @@ isolate_full_html <- function(data, metadata = NULL) {
     metadata
 }
 
+#' @importFrom methods is
+read_all <- function(file, isbinary) {
+    read <- if (isbinary)
+        function(s) readBin(file, 'raw', s)
+    else
+        function(s) readChar(file, s)
+    
+    size <- tryCatch(file.info(file)$size, error = function(e) NULL)
+    
+    if (!is.null(size)) {
+        read(file, size)
+    } else {
+        if (is.character(file)) file <- base::file(file, 'rb')
+        stopifnot(is(file, 'connection'))
+        if (!isOpen(file)) open(file)
+        rv <- if (isbinary) raw() else character()
+        chunk <- read(1024)
+        while (length(chunk) > 0) {
+            rv <- c(rv, chunk)
+            chunk <- read(1024)
+        }
+        rv
+    }
+}
